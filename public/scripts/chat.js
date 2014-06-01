c619b4cb4386608f82ba9a5cc25342da5c779dcc
juliet <juliet.zhu.work@gmail.com>
2014-06-01 23:17:27 +0800

    working with namespace.

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,27 +1,34 @@
-var socket = io.connect('/');
-socket.on('name_set',function(data){
-		$('#nameform').hide();
-		$('#messages').append('<div class="systemMessage">' + 'Hello '+data.name+'</div>');
+var chatInfra = io.connect('/chat_infra'),
+	chatCom = io.connect('/chat_com');
+chatInfra.on('name_set',function(data){
+	$('#messages').append('<div>Hi,'+data.name+'</div>');
+	chatInfra.on('user_entered',function(user){
+		$('#messages').append('<div class="systemMessage">'+user.name+' has joined the room.'+'</div>' )
 	});
-socket.on('message',function(data){
-	data = JSON.parse(data);
-	if(data.username){
-		$('#messages').append('<div class="'+data.type+'"><span class="name">' +data.username + ":</span> " +data.message + '</div>');
-	}else{
-		$('#messages').append('<div class="'+data.type+'">' +data.message +'</div>');
-	}
+
 });
+chatInfra.on('message', function (message) {
+	message = JSON.parse(message);
+   $('#messages').append('<div class="' + message.type + '">' + message.message + '</div>');
+});
+
+chatCom.on('message',function(message){
+	message = JSON.parse(message);
+    $('#messages').append('<div class="' + message.type + '"><span class="name">' + message.username + ':</span> ' + message.message + '</div>');
+});
+
 $(function(){
 	$('#send').click(function(){
 		var data = {
 			message:$('#message').val(),
 			type:"userMessage"
 		};
-		socket.send(JSON.stringify(data));
-		$('#message').val();
+		chatCom.send(JSON.stringify(data));
+		$('#message').val('');
 	});
 	$('#setname').click(function(){
-		socket.emit('set_name',{name:$('#nickname').val()});
+		chatInfra.emit("set_name", {name:$('#nickname').val()});
+		$('#nameform').hide();
 	});
 	
 });
--- routers/sockets.js
+++ routers/sockets.js
@@ -1,25 +1,28 @@
 var io = require('socket.io');
 exports.initialize = function (server){
 	io = io.listen(server);
-	io.sockets.on('connection',function (socket){
-		socket.send(JSON.stringify({type:"serverMessage",message:"Welcome to the most intersting chatroom on the earth!"}));
-		socket.on('message',function(message){
-			message = JSON.parse(message);
-			if(message.type=="userMessage"){
-				socket.get('nickname', function(err, nickname){
-					message.username=nickname;
-					socket.broadcast.send(JSON.stringify(message));
-					message.type = "myMessage";
-					socket.send(JSON.stringify(message));
-				});
-			}
-		});
+	var chatInfra = io.of('/chat_infra').on('connection',function(socket){//ctreate a new namespace
 		socket.on('set_name',function(data){
 			socket.set('nickname',data.name,function(){
 				socket.emit('name_set',data);
-			});
-			socket.send(JSON.stringify({type:"serverMessage",message:"Welcome to the most interesting chat room on earth!"}));
+				socket.send(JSON.stringify({type:'serverMessage',message:'Welcome to the most interesting ' + 'chat room on earth!'}));
+				
+				socket.broadcast.emit('user_entered',data);
+			})
 		})
-	});
+	})
+	var chatCom = io.of('/chat_com').on('connection',function(socket){
+		socket.on('message',function(message){
+			message = JSON.parse(message);
+			if(message.type='userMessage'){
+				socket.get('nickname',function(err,nickname){
+					message.username = nickname;
+					socket.broadcast.send(JSON.stringify(message));
+					message.type = 'myMessage';
+					socket.send(JSON.stringify(message));
+				})
+			}
+		})
+	})
 
 }
